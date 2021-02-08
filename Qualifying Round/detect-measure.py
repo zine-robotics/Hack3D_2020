566f7e63e642382b1b185a80fdf5b0904eff0c6c
Chirayu Rankawat <57005734+ChirayuR@users.noreply.github.com>
2021-02-08 19:05:23 +0530

    Update detect-measure.py

--- Qualifying Round/detect-measure.py
+++ Qualifying Round/detect-measure.py
@@ -8,9 +8,11 @@ extracted_img = 'extract1.jpg'
 
 img = Image.open(filename)
 exif_dict = piexif.load(img.info['exif'])
+#loads all exif data
 
 user_comment = piexif.helper.UserComment.load(exif_dict["Exif"][piexif.ExifIFD.UserComment])
+#extracts the user comment(edited exif data) from the given picture.
 #print(user_comment)
 
 with open(extracted_img,'wb') as imageFile: imageFile.write(base64.b64decode(user_comment.encode('utf-8')))
-
+#writes the user-commented exif data into an image file(extracted_img)
